95eed755fffd6c782c2279e310956c3d9573961d
Jesse Mather <jmather@arista.com>
2018-02-20 20:07:17 -0800

    add tests for cerificates

--- test_eapi.py
+++ test_eapi.py
@@ -3,25 +3,34 @@
 # Arista Networks, Inc. Confidential and Proprietary.
 
 import eapi
-import pytest
 import os
+import pytest
+
+eapi.EAPI_SSL_WARNINGS = False
 
 EAPI_HOST = os.environ.get('EAPI_HOST', "veos1")
-
-# @pytest.fixture()
-# def session():
-#     return
+EAPI_CLIENT_CERT = os.environ.get('EAPI_CLIENT_CERT')
+EAPI_CLIENT_KEY = os.environ.get('EAPI_CLIENT_KEY')
 
 def test_execute():
-    sess = eapi.session(EAPI_HOST, auth=("admin", ""))
+    sess = eapi.session(EAPI_HOST)
     response = sess.execute(["show hostname"])
-
-def test_ssl_noverify():
-    sess = eapi.session(EAPI_HOST, transport="https", verify=False)
-    sess.execute(["show hostname"])
 
 def test_ssl_verify():
     sess = eapi.session(EAPI_HOST, transport="https", verify=True)
 
     with pytest.raises(eapi.EapiError):
         sess.execute(["show hostname"])
+
+def test_ssl_noverify():
+    sess = eapi.session(EAPI_HOST, transport="https", verify=False)
+    sess.execute(["show hostname"])
+
+def test_ssl_client_cert():
+    if not (EAPI_CLIENT_CERT and EAPI_CLIENT_KEY):
+        pytest.skip("certificate/key pair is not set")
+
+    sess = eapi.session(EAPI_HOST, cert=(EAPI_CLIENT_CERT, EAPI_CLIENT_KEY),
+                        transport="https", verify=False)
+
+    sess.execute(["show hostname"])
