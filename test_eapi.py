bf0a0335b88f635cf5f6c38d721fb9e69d269f3d
Jesse Mather <jmather@arista.com>
2018-02-21 22:07:57 -0800

    add tests

--- test_eapi.py
+++ test_eapi.py
@@ -5,20 +5,44 @@
 import eapi
 import os
 import pytest
+import time
 
 eapi.SSL_WARNINGS = False
 
 EAPI_HOST = os.environ.get('EAPI_HOST', "veos1")
 EAPI_CLIENT_CERT = os.environ.get('EAPI_CLIENT_CERT')
 EAPI_CLIENT_KEY = os.environ.get('EAPI_CLIENT_KEY')
+commands = ["show hostname"]
 
 def test_execute():
     sess = eapi.session(EAPI_HOST)
-    response = sess.execute(["show hostname"])
+    response = sess.execute(commands)
 
     response.raise_for_error()
 
     assert response.code == 0, "Expected a clean response"
+
+def test_response():
+    time.sleep(5)
+    with eapi.session(EAPI_HOST) as sess:
+        response = sess.execute(commands)
+
+        assert hasattr(response, "output")
+
+        assert "output" in response.to_dict()
+
+def test_with_context():
+    with eapi.session(EAPI_HOST) as sess:
+        sess.execute(commands)
+#
+def test_login():
+    sess = eapi.session(EAPI_HOST)
+    sess.login()
+    assert sess.logged_in, "expected to be logged in..."
+
+def test_prepare_url():
+    sess = eapi.session(EAPI_HOST)
+    sess.prepare_url(path="/fake-path")
 
 def test_invalid_login():
     sess = eapi.session(EAPI_HOST, auth=("baduser", "baspass"))
@@ -35,15 +59,21 @@ def test_execute_bad_command():
 
     assert response.code == 1002, "Expected an errored response"
 
+def test_ssl_verify_setter():
+
+    with pytest.raises(TypeError):
+        sess = eapi.session(EAPI_HOST, transport="https")
+        sess.verify = "No"
+
 def test_ssl_verify():
     sess = eapi.session(EAPI_HOST, transport="https", verify=True)
 
     with pytest.raises(eapi.EapiError):
-        sess.execute(["show hostname"])
+        sess.execute(commands)
 
 def test_ssl_noverify():
     sess = eapi.session(EAPI_HOST, transport="https", verify=False)
-    sess.execute(["show hostname"])
+    sess.execute(commands)
 
 def test_ssl_client_cert():
     if not (EAPI_CLIENT_CERT and EAPI_CLIENT_KEY):
@@ -52,4 +82,4 @@ def test_ssl_client_cert():
     sess = eapi.session(EAPI_HOST, cert=(EAPI_CLIENT_CERT, EAPI_CLIENT_KEY),
                         transport="https", verify=False)
 
-    sess.execute(["show hostname"])
+    sess.execute(commands)
