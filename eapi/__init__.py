e18566b400a577e803162f401dca4e9dc52287f2
Jesse Mather <jmather@arista.com>
2020-04-27 11:26:34 -0700

    version bump

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 # from eapi.constants import ENCODING, INCLUDE_TIMESTAMPS, SSL_VERIFY, \
 #                          SSL_WARNINGS, TIMEOUT
