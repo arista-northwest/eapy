0529d19dcf7636adec482329385cb4a99ce09cd5
Jesse Mather <jmather@arista.com>
2020-04-29 19:57:52 -0700

    switch to httpx

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
 
-__version__ = "0.5.7"
+__version__ = "0.6.0"
 
 # from eapi.constants import ENCODING, INCLUDE_TIMESTAMPS, SSL_VERIFY, \
 #                          SSL_WARNINGS, TIMEOUT
